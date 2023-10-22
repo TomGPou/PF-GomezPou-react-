a3912f0aaa4e90047e035f7412ad3735deb257ca
tomasgpou <tomas.gomezpou@gmail.com>
2023-10-22 08:39:49 -0300

    se crea ruteo componetizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
 import { customTheme } from "./components/common/themeConfig/themeconfig";
-import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
-import Layout from "./components/layout/layout";
-import ItemDetailContainer from "./components/pages/itemDetailContainer/ItemDetailContainer";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<ItemListContainer />} />
-            <Route
-              path="/category/:categoryName"
-              element={<ItemListContainer />}
-            />
-
-            <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
-
-            <Route path="/cart" element={<h1>Este es el carrito</h1>} />
-          </Route>
-
-          <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes>
+        <AppRouter />
       </ThemeProvider>
     </BrowserRouter>
   );
